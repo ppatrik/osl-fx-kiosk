41e9bd064dcde537f68f5a1e3ffce4c467b3a85e
ulfada <ulfada@d5f24ca5-d533-0410-9008-d7335e2ce2c0>
2007-06-26 21:43:05 +0000

    adding first files for prototype

---
+++ trunk/init.js
@@ -0,0 +1,39 @@
+var contentBrowser;
+contentBrowser = document.getElementById("contentBrowser");
+
+function init(){
+	//will be true later
+	window.fullScreen = false;
+}
+
+function backClicked(){
+	try{
+	alert(contentBrowser);
+	alert(document.getElementById("contentBrowser").goBack);
+	alert(document.getElementById("contentBrowser").webNavigation);
+	if(contentBrowser.webNavigation.canGoBack == true)
+		contentBrowser.webNavigation.goBack();
+	}
+	catch(e){
+	alert("exception in backClicked: " + e);
+	}
+}
+
+function forwardClicked(){
+	try{
+	if(document.getElementById("contentBrowser").canGoForward == true)
+		document.getElementById("contentBrowser").goForward();
+	}
+	catch(e){
+	alert("exception in backClicked: " + e);
+	}
+}
+
+function homeClicked(){
+	try{
+	document.getElementById("contentBrowser").goHome();
+	}
+	catch(e){
+	alert("exception in homeClicked: " + e);
+	}
+}
