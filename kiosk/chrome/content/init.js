0b3af433b0144c657b6defdd58b44f4a7ddc9955
ulfada <ulfada@d5f24ca5-d533-0410-9008-d7335e2ce2c0>
2007-07-26 06:17:29 +0000

    should now pop back to normal size if resized in Windows

--- kiosk/chrome/content/init.js
+++ kiosk/chrome/content/init.js
@@ -1,14 +1,32 @@
 var contentBrowser = null;
 var Ci = Components.interfaces;
+var width;
+var height;
 
 function init(){
 	setTimeout(function(){
 		fullScreen = true;
 	}, 0);
+	//set resize event
+	window.addEventListener("focus", onFocus, false);
+	window.addEventListener("resize", onResize, false);
 	contentBrowser = document.getElementById("contentBrowser");
 	//these might be needed when using a tabbrowser
 	//contentBrowser.homePage = "http://www.osuosl.org";
 	//contentBrowser.goHome();
+}
+
+function onFocus(){
+	//if width and height aren't cached, cache them
+	if(!(width > 1) || !(height > 1)){
+		width = window.outerWidth;
+		height = window.outerHeight;
+	}
+}
+
+function onResize(){
+	//put it back to normal size
+	window.resizeTo(width, height);
 }
 
 function backClicked(){
--- kiosk/defaults/preferences/prefs.js
+++ kiosk/defaults/preferences/prefs.js
@@ -1,1 +1,2 @@
 pref("toolkit.defaultChromeURI", "chrome://kiosk/content/kiosk.xul");
+pref("javascript.options.showInConsole", true);
