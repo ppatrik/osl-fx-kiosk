0640ffab4ad352659f108008a57846e7e3c20035
ulfada <ulfada@d5f24ca5-d533-0410-9008-d7335e2ce2c0>
2007-09-23 02:04:38 +0000

    trying to get it to stop showing the "entering secure site" warning

--- odekiosk/defaults/preferences/prefs.js
+++ odekiosk/defaults/preferences/prefs.js
@@ -1,3 +1,5 @@
 pref("toolkit.defaultChromeURI", "chrome://kiosk/content/kiosk.xul");
 pref("javascript.options.showInConsole", true);
 pref("kiosk.hideTools", true);
+pref("security.warn_entering_secure.show_once", false);
+pref("security.warn_entering_secure", false);
